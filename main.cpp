1ea7d3e2ec32bc51c8cfe93272857b811737cdab
Alexandre Marchioli <alexandresilva.marchioli@gmail.com>
2025-02-04 19:55:02 -0300

    second

--- main.cpp
+++ main.cpp
@@ -2,12 +2,18 @@
 #include "NeuronioReLU.hpp"
 using namespace std;
 
-int main(){
-    NeuronioReLU neuronio1({0.2,0.4}, -1.5);
-    cout << neuronio1.predict({0.3,2.0}) << endl;
+int main() {
+    Neuronio* neuronio1 = new NeuronioReLU({0.3, 0.7}, 0.5);
+    Neuronio* neuronio2 = new NeuronioReLU({0.1, 0.1}, -0.2);
+    Neuronio* neuronio3 = new NeuronioReLU({-0.2, -0.4}, -0.5);
 
-    NeuronioReLU neuronio2({0.2,0.4}, -0.5);
-    cout << neuronio2.predict({0.3,2.0}) << endl;
+    cout << neuronio1->predict({0.4, 0.5}) << endl; //Resultado positivo, logo, imprime o valor
+    cout << neuronio2->predict({1.0, 1.0}) << endl; //Resultado nulo, logo, imprime 0
+    cout << neuronio3->predict({1.0, 1.0}) << endl; //Resultado negativo (-1.1), logo, imprime 0
+
+    delete neuronio1;
+    delete neuronio2;
+    delete neuronio3;
 
     return 0;
 }
